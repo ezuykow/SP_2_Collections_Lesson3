27c589cfd9531d47623bd6425e2418d5b4d00964
ezuykow <zuy1996@gmail.com>
2022-10-19 13:33:26 +0300

    done

---
+++ src/PhoneNumbers.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneNumbers {
+
+    private Map<String, String> map;
+
+    public PhoneNumbers() {
+        map  = new HashMap<>(20);
+    }
+
+    public void add(String name, String num) {
+        map.put(name, num);
+    }
+
+    public void showAll() {
+        map.forEach((k, v) -> System.out.printf("%s %s\n", k, v));
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        task1();
+
+        task3();
+
+        task2_1();
+
+        task2_2();
+    }
+
+    private static void task2_2() {
+        Map<Integer, String> map = new TreeMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            map.put(i + 1, "str" + i);
+        }
+
+        map.forEach((k, v) -> System.out.println(k + " " + v));
+    }
+
+    private static void task2_1() {
+        Map<String, List<Integer>> map = new HashMap<>();
+        fillMap(map);
+
+        Map<String, Integer> newMap = new HashMap<>();
+
+        for (Map.Entry<String, List<Integer>> e : map.entrySet()) {
+            newMap.put(e.getKey(), e.getValue().stream().mapToInt(i -> i).sum());
+        }
+
+        newMap.forEach((k, v) -> System.out.println(k + " " + v));
+    }
+
+    private static void fillMap(Map<String, List<Integer>> map) {
+        Random r = new Random();
+        for (int i = 0; i < 5; i++) {
+            map.put(("str" + (i + 1)), List.of(r.nextInt(1000), r.nextInt(1000), r.nextInt(1000)));
+        }
+    }
+
+    private static void task3() {
+        Map<String, Integer> map = new HashMap<>();
+
+        String key = "123";
+        int value = 123;
+        add(key, value, map);
+    }
+
+    private static void add(String k, int v, Map<String, Integer> map) {
+        if (map.containsKey(k) && map.get(k).equals(v)) {
+            throw new IllegalArgumentException();
+        }
+        map.put(k, v);
+    }
+
+    private static void task1() {
+        var phoneNumbers = new PhoneNumbers();
+        fillPhoneNumbers(phoneNumbers);
+        phoneNumbers.showAll();
+    }
+
+    private static void fillPhoneNumbers(PhoneNumbers pn) {
+        for (int i = 0; i < 20; i++) {
+            pn.add(("Name" + i), String.valueOf(i).repeat(6));
+        }
+    }
+}
